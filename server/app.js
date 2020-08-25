3647f4b553931a9ae614d99efce7d0bb6c2e751c
Arl9quine <alexander.vorobev@outlook.com>
2020-08-25 11:10:40 -0400

    just comments

--- server/app.js
+++ server/app.js
@@ -8,3 +8,11 @@ let port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`The Server is listening at port: ${port}.`);
 });
+
+// Webscraper sends an array of links and I send them to all users
+
+// Client registers with: email and password
+
+// This method checks if the user has ok email/pass
+
+// inserts a user with username, email, password, subjects array, sites array, date, link array
