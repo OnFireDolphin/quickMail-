1173032c528de08861e0449d920954b5a48d9bfb
Kyrollos <kirobekhet@gmail.com>
2020-08-25 16:40:58 -0400

    adding a functional script

--- scraper/script.py
+++ scraper/script.py
@@ -1,12 +1,11 @@
 from newsapi import NewsApiClient
 
 # Init
-newsapi = NewsApiClient(api_key='Your Key')
+newsapi = NewsApiClient(api_key='8bffb1c08ad0417684c8be82224a46a2')
 
 # /v2/top-headlines
 top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge',
+                                          category='business',
                                           language='en',
-                                          )
-
+                                          country='us')
 print(top_headlines)
