31174d38959e730bed7f48f1f05f3881d2cb9cb2
Arl9quine <alexander.vorobev@outlook.com>
2020-08-25 11:05:10 -0400

    the server is now listening on 5k

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`The Server is listening at port: ${port}.`);
+});
